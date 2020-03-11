22d2730b03a0a19e09e69c038e1c38edc746e6b6
神樹桜乃 <kodama@sakuno.moe>
2020-03-11 17:35:20 +0800

    Move jest configuration to a single file

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  globals: {
+    "ts-jest": {
+      packageJson: "package.json",
+    },
+  },
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "ts"
+  ],
+  rootDir: "src",
+  testRegex: ".spec.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  moduleNameMapper: {
+    "^src/(.+)": "<rootDir>/$1",
+  },
+  coverageDirectory: "coverage",
+  testEnvironment: "node"
+};
