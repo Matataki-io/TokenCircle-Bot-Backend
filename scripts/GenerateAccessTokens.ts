b8e6b3fcde9c381c7a0e78828d50efcc862318b5
韦海晖 <franciswei98@gmail.com>
2020-03-11 16:07:17 +0800

    feat: script to generate access token

---
+++ scripts/GenerateAccessTokens.ts
@@ -0,0 +1,23 @@
+import { getConnectionOptions, createConnection } from "typeorm";
+import { randomBytes } from "crypto";
+
+async function generateAccessToken() {
+    const options = await getConnectionOptions()
+    if (options.type !== "postgres") {
+        console.error("Require PostgreSQL database");
+        return;
+    }
+    const token = randomBytes(48).toString('hex')
+    const sqlCommand = `insert into "${options.schema}".access_bearer_tokens(token) VALUES('${token}')`;
+    const conn = await createConnection();
+
+    const queryRunner = conn.createQueryRunner();
+    await queryRunner.query(sqlCommand, undefined);
+
+    console.info(`Your new access token is: ${token}`)
+    await conn.close()
+    process.exit();
+
+}
+
+generateAccessToken()
