a4d35de041327c6648426c28ce594cdb25b35e98
神樹桜乃 <kodama@sakuno.moe>
2020-03-13 18:41:38 +0800

    Renew get mapping part

--- src/get-mapping/get-mapping.controller.spec.ts
+++ src/get-mapping/get-mapping.controller.spec.ts
@@ -33,4 +33,52 @@ describe('GetMapping Controller', () => {
     it('should be defined', () => {
         expect(controller).toBeDefined();
     });
+
+    test("Get user 1 by his/her telegram id", async () => {
+        const { data } = await controller.getUserByTelegramId(1);
+
+        expect(data).toStrictEqual(Object.assign(new User(), {
+            id: 1,
+            name: "User 1",
+            telegramUid: 1,
+            walletAddress: "0x1",
+        }));
+    });
+    test("Get nothing by invalid telegram id", () => {
+        expect(controller.getUserByTelegramId(-1)).rejects.toThrowError("No user record for telegram uid: -1");
+    });
+
+    test("Get FST token", async () => {
+        const { data } = await controller.getTokenBySymbol("FST");
+
+        expect(data).toStrictEqual(Object.assign(new Token(), {
+            id: 1,
+            name: "Token 1",
+            symbol: "FST",
+            contractAddress: "0x1",
+            issuer: Object.assign(new User(), {
+                id: 1,
+                name: "User 1",
+                telegramUid: 1,
+                walletAddress: "0x1",
+            }),
+        }));
+    });
+    test("Get nothing by invalid token", () => {
+        expect(controller.getTokenBySymbol("INVALID")).rejects.toThrowError("No record for Token symbol: INVALID");
+    });
+
+    test("Get FST token by user 1", async () => {
+        const { data } = await controller.getTokenByUserId(1);
+
+        expect(data).toStrictEqual([Object.assign(new Token(), {
+            id: 1,
+            name: "Token 1",
+            symbol: "FST",
+            contractAddress: "0x1",
+        })]);
+    });
+    test("Get nothing by invalid user id", () => {
+        expect(controller.getTokenByUserId(-1)).rejects.toThrowError("No record for Token of the user: -1");
+    });
 });
--- src/get-mapping/get-mapping.controller.ts
+++ src/get-mapping/get-mapping.controller.ts
@@ -1,52 +1,59 @@
-import { Controller, Get, UseGuards, Param, BadRequestException, NotFoundException, NotImplementedException } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
-import { BearerGuard } from 'src/auth/bearer.guard';
-import { TokenService } from 'src/token/token.service';
+import { Controller, Get, UseGuards, Param, BadRequestException, NotFoundException, ParseIntPipe } from "@nestjs/common";
+import { UserService } from "src/user/user.service";
+import { BearerGuard } from "src/auth/bearer.guard";
+import { TokenService } from "src/token/token.service";
 import { getTokenByUserIdResponse, getTokenBySymbolResponse,
     getTgUidToUserResponse } from "./response.types";
 import { ApiTags, ApiSecurity, ApiOperation, ApiOkResponse,
-    ApiParam, ApiUnauthorizedResponse } from '@nestjs/swagger';
+    ApiParam, ApiUnauthorizedResponse } from "@nestjs/swagger";
 
-@ApiTags('mapping')
-@ApiSecurity('bearer')
+@ApiTags("mapping")
+@ApiSecurity("bearer")
 @UseGuards(BearerGuard)
 @ApiUnauthorizedResponse({ description: "You will get 401 if request headers is without Bearer token."})
-@Controller('mapping')
+@Controller("mapping")
 export class GetMappingController {
     constructor(
         private readonly userService: UserService,
         private readonly tokenService: TokenService
     ) { }
 
-    @ApiOperation({ summary: 'Get User profile with user-binded Telegram ID`' })
+    @ApiOperation({ summary: "Get User profile with user-binded Telegram ID`" })
     @ApiParam({ name: "telegramUid", description: "Telegram User ID" })
     @ApiOkResponse({ description: "Return the user of provided telegram uid", type: getTgUidToUserResponse})
-    @Get('/telegramUidToUser/:telegramUid')
-    async getUserByTelegramId(@Param('telegramUid') telegramUid: string) {
-        if (isNaN(Number(telegramUid))) throw new BadRequestException('"telegramUid" should be a number');
-        const user = await this.userService.getUserByTelegramUid(telegramUid)
-        if (!user) throw new NotFoundException(`No user record for telegram uid: ${telegramUid}`)
-        return { user }
+    @Get("/telegramUidToUser/:telegramUid")
+    async getUserByTelegramId(@Param("telegramUid", ParseIntPipe) telegramUid: number) {
+        const user = await this.userService.getUserByTelegramUid(telegramUid);
+        if (!user) throw new NotFoundException(`No user record for telegram uid: ${telegramUid}`);
+
+        return {
+            data: user,
+        };
     }
 
-    @ApiOperation({ summary: 'should be used with `getMinetokenIdFromSymbol`' })
+    @ApiOperation({ summary: "should be used with `getMinetokenIdFromSymbol`" })
     @ApiParam({ name: "symbol", description: "Matataki Token Symbol (For now)" })
     @ApiOkResponse({ description: "Return the token of the symbol", type: getTokenBySymbolResponse})
-    @Get('/symbolToToken/:symbol')
-    async getTokenBySymbol(@Param('symbol') symbol: string) {
-        const token = await this.tokenService.getTokenBySymbol(symbol)
-        if (!token) throw new NotFoundException(`No record for Token symbol: ${symbol}`)
-        return { token }
+    @Get("/symbolToToken/:symbol")
+    async getTokenBySymbol(@Param("symbol") symbol: string) {
+        const token = await this.tokenService.getTokenBySymbol(symbol);
+        if (!token) throw new NotFoundException(`No record for Token symbol: ${symbol}`);
+
+        return {
+            data: token,
+        };
     }
 
-    @ApiOperation({ summary: 'should be used with `getUserMinetoken`' })
+    @ApiOperation({ summary: "should be used with `getUserMinetoken`" })
     @ApiParam({ name: "userId", description: "Matataki User ID"})
     @ApiOkResponse({ description: "Return the token issued by user", type: getTokenByUserIdResponse})
-    @Get('/userToToken/:userId')
-    async getTokenByUserId(@Param('userId') userId: string) {
-        if (isNaN(Number(userId))) throw new BadRequestException('"telegramUid" should be a number');
-        const user = await this.userService.get(Number(userId));
-        if (!user) throw new NotFoundException(`No record for Token of the user: ${userId}`)
-        return { issuedTokens: user.issuedTokens }
+    @Get("/userToToken/:userId")
+    async getTokenByUserId(@Param("userId", ParseIntPipe) userId: number) {
+        const user = await this.userService.get(userId);
+        if (!user) throw new NotFoundException(`No record for Token of the user: ${userId}`);
+
+        return {
+            data: user.issuedTokens
+        };
     }
 }
